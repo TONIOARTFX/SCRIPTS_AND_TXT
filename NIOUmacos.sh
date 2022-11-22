c73e68d50ccca1f35672904d081e1c579f9f83db
TONIOARTFX <tonio@artifices.eu>
2022-11-22 12:28:50 +0100

    221122A

--- NIOUmacos.sh
+++ NIOUmacos.sh
@@ -16,10 +16,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "Example"
-sudo scutil --set HostName "Example"
-sudo scutil --set LocalHostName "Example"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "Example"
+# sudo scutil --set ComputerName "Example"
+# sudo scutil --set HostName "Example"
+# sudo scutil --set LocalHostName "Example"
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "Example"
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
